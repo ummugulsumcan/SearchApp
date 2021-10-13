773f90f5fe6f16a3cab0a76a24d0aa557f8c257f
ummugulsumcann <ummu_can_95@hotmail.com>
2021-10-13 17:57:44 +0300

    datajs added

---
+++ data.js
@@ -0,0 +1,513 @@
+module.exports = function () {
+  return {
+
+    data: [
+
+        {
+          id: "1",
+          name: "Game 1",
+          bundle: "bundle_1",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 1",
+          state: "TEST"
+        },
+        {
+          id: "2",
+          name: "Game 2",
+          bundle: "bundle_2",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 2",
+          state: "TEST"
+        },
+        {
+          id: "3",
+          name: "Game 3",
+          bundle: "bundle_3",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 3",
+          state: "TEST"
+        },
+        {
+          id: "4",
+          name: "Game 4",
+          bundle: "bundle_4",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 4",
+          state: "TEST"
+        },
+        {
+          id: "5",
+          name: "Game 5",
+          bundle: "bundle_5",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 5",
+          state: "TEST"
+        },
+        {
+          id: "6",
+          name: "Game 6",
+          bundle: "bundle_6",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 6",
+          state: "TEST"
+        },
+        {
+          id: "7",
+          name: "Game 7",
+          bundle: "bundle_7",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 7",
+          state: "TEST"
+        },
+        {
+          id: "8",
+          name: "Game 8",
+          bundle: "bundle_8",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 8",
+          state: "TEST"
+        },
+        {
+          id: "9",
+          name: "Game 9",
+          bundle: "bundle_9",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 9",
+          state: "TEST"
+        },
+        {
+          id: "10",
+          name: "Game 10",
+          bundle: "bundle_10",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 10",
+          state: "TEST"
+        },
+        {
+          id: "11",
+          name: "Game 11",
+          bundle: "bundle_11",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 11",
+          state: "TEST"
+        },
+        {
+          id: "12",
+          name: "Game 12",
+          bundle: "bundle_12",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 12",
+          state: "TEST"
+        },
+        {
+          id: "13",
+          name: "Game 13",
+          bundle: "bundle_13",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 13",
+          state: "TEST"
+        },
+        {
+          id: "14",
+          name: "Game 14",
+          bundle: "bundle_14",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 14",
+          state: "TEST"
+        },
+        {
+          id: "15",
+          name: "Game 15",
+          bundle: "bundle_15",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 15",
+          state: "TEST"
+        },
+        {
+          id: "16",
+          name: "Game 16",
+          bundle: "bundle_16",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 16",
+          state: "TEST"
+        },
+        {
+          id: "17",
+          name: "Game 17",
+          bundle: "bundle_17",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 17",
+          state: "TEST"
+        },
+        {
+          id: "18",
+          name: "Game 18",
+          bundle: "bundle_18",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 18",
+          state: "TEST"
+        },
+        {
+          id: "19",
+          name: "Game 19",
+          bundle: "bundle_19",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 19",
+          state: "TEST"
+        },
+        {
+          id: "20",
+          name: "Game 20",
+          bundle: "bundle_20",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 20",
+          state: "TEST"
+        },
+        {
+          id: "21",
+          name: "Game 21",
+          bundle: "bundle_21",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 21",
+          state: "TEST"
+        },
+        {
+          id: "22",
+          name: "Game 22",
+          bundle: "bundle_22",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 22",
+          state: "TEST"
+        },
+        {
+          id: "23",
+          name: "Game 23",
+          bundle: "bundle_23",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 23",
+          state: "TEST"
+        },
+        {
+          id: "24",
+          name: "Game 24",
+          bundle: "bundle_24",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 24",
+          state: "TEST"
+        },
+        {
+          id: "25",
+          name: "Game 25",
+          bundle: "bundle_25",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 25",
+          state: "TEST"
+        },
+        {
+          id: "26",
+          name: "Game 26",
+          bundle: "bundle_26",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 26",
+          state: "TEST"
+        },
+        {
+          id: "27",
+          name: "Game 27",
+          bundl: "bundle_27",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 27",
+          state: "TEST"
+        },
+        {
+          id: "28",
+          name: "Game 28",
+          bundle: "bundle_28",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 28",
+          state: "TEST"
+        },
+        {
+          id: "29",
+          name: "Game 29",
+          bundle: "bundle_29",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 29",
+          state: "TEST"
+        },
+        {
+          id: "30",
+          name: "Game 30",
+          bundle: "bundle_30",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 30",
+          state: "TEST"
+        },
+        {
+          id: "31",
+          name: "Game 31",
+          bundle: "bundle_31",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 31",
+          state: "TEST"
+        },
+        {
+          id: "32",
+          name: "Game 32",
+          bundle: "bundle_32",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 32",
+          state: "TEST"
+        },
+        {
+          id: "33",
+          name: "Game 33",
+          bundle: "bundle_33",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 33",
+          state: "TEST"
+        },
+        {
+          id: "34",
+          name: "Game 34",
+          bundle: "bundle_34",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 34",
+          state: "TEST"
+        },
+        {
+          id: "35",
+          name: "Game 35",
+          bundle: "bundle_35",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 35",
+          state: "TEST"
+        },
+        {
+          id: "36",
+          name: "Game 36",
+          bundle: "bundle_36",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 36",
+          state: "TEST"
+        },
+        {
+          id: "37",
+          name: "Game 37",
+          bundle: "bundle_37",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 37",
+          state: "TEST"
+        },
+        {
+          id: "38",
+          name: "Game 38",
+          bundle: "bundle_38",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 38",
+          state: "TEST"
+        },
+        {
+          id: "39",
+          name: "Game 39",
+          bundle: "bundle_39",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 39",
+          state: "TEST"
+        },
+        {
+          id: "40",
+          name: "Game 40",
+          bundle: "bundle_40",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 40",
+          state: "TEST"
+        },
+        {
+          id: "41",
+          name: "Game 41",
+          bundle: "bundle_41",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 41",
+          state: "TEST"
+        },
+        {
+          id: "42",
+          name: "Game 42",
+          bundle: "bundle_42",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 42",
+          state: "TEST"
+        },
+        {
+          id: "43",
+          name: "Game 43",
+          bundle: "bundle_43",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 43",
+          state: "TEST"
+        },
+        {
+          id: "44",
+          name: "Game 44",
+          bundle: "bundle_44",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 44",
+          state: "TEST"
+        },
+        {
+          id: "45",
+          name: "Game 45",
+          bundle: "bundle_45",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 45",
+          state: "TEST"
+        },
+        {
+          id: "46",
+          name: "Game 46",
+          bundle: "bundle_46",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 46",
+          state: "TEST"
+        },
+        {
+          id: "47",
+          name: "Game 47",
+          bundle: "bundle_47",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 47",
+          state: "TEST"
+        },
+        {
+          id: "48",
+          name: "Game 48",
+          bundle: "bundle_48",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 48",
+          state: "TEST"
+        },
+        {
+          id: "49",
+          name: "Game 49",
+          bundle: "bundle_49",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 49",
+          state: "TEST"
+        },
+        {
+          id: "50",
+          name: "Game 50",
+          bundle: "bundle_50",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 50",
+          state: "TEST"
+        },
+        {
+          id: "51",
+          name: "Game 51",
+          bundle: "bundle_51",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 51",
+          state: "TEST"
+        },
+        {
+          id: "52",
+          name: "Game 52",
+          bundle: "bundle_52",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 52",
+          state: "TEST"
+        },
+        {
+          id: "53",
+          name: "Game 53",
+          bundle: "bundle_53",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 53",
+          state: "TEST"
+        },
+        {
+          id: "54",
+          name: "Game 54",
+          bundle: "bundle_54",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 54",
+          state: "TEST"
+        },
+        {
+          id: "55",
+          name: "Game 55",
+          bundle: "bundle_55",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 55",
+          state: "TEST"
+        },
+        {
+          id: "56",
+          name: "Game 56",
+          bundle: "bundle_56",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 56",
+          state: "TEST"
+        },
+        {
+          id: "57",
+          name: "Game 57",
+          bundle: "bundle_57",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 57",
+          state: "TEST"
+        },
+        {
+          id: "58",
+          name: "Game 58",
+          bundle: "bundle_58",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 58",
+          state: "TEST"
+        },
+        {
+          id: "59",
+          name: "Game 59",
+          bundle: "bundle_59",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 59",
+          state: "TEST"
+        },
+        {
+          id: "60",
+          name: "Game 60",
+          bundle: "bundle_60",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 60",
+          state: "TEST"
+        },
+        {
+          id: "61",
+          name: "Game 61",
+          bundle: "bundle_61",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 61",
+          state: "TEST"
+        },
+        {
+          id: "62",
+          name: "Game 62",
+          bundle: "bundle_62",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 62",
+          state: "TEST"
+        },
+        {
+          id: "63",
+          name: "Game 63",
+          bundle: "bundle_63",
+          icon: "https://dummyimage.com/512x512/aaa/000",
+          studio: "Studio 63",
+          state: "TEST"
+        }
+    ]
+
+  }
+}
